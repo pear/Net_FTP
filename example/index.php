91ea9b62a91f9762f5bdff76635d390e476d859f
Tobias Schlitt <toby@php.net>
2004-01-05 22:47:38 +0000

    * Updated to new test cases.
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_FTP/trunk@147919 c90b9560-bf6c-de11-be94-00142212c4b1

--- example/index.php
+++ example/index.php
@@ -1,7 +1,13 @@
 <?php
 
+	ini_set('include_path', '/cvs/pear/Net_FTP:'.ini_get('include_path'));
+
+    $hostname = "example.com";
+    $user = "xxx";
+    $pass = "xxx";
+
     function dump ( $var, $desc = null ) {
-    
+
         if (isset($desc)) {
             echo "<br><b>$desc</b>";
         }
@@ -11,47 +17,30 @@
     }
 
     require_once 'Net/FTP.php';
-    
-    // $ftp = new Net_FTP('localhost', 21);
+
     $ftp = new Net_FTP();
-    
-    dump($ftp->setHostname('localhost'), 'FTP::setHostname()');
+    dump($ftp->setHostname($hostname), 'FTP::setHostname()');
     dump($ftp->setPort(21), 'FTP::setPort()');
     dump($ftp->connect(), 'FTP::connect()');
-    // dump($ftp->connect('localhost', 21), 'FTP::connect()');
-
-    dump($ftp->setUsername('xxx'), 'FTP::setUsername()');
-    dump($ftp->setPassword('xxx'), 'FTP::setPassword()');
+    dump($ftp->setUsername($user), 'FTP::setUsername()');
+    dump($ftp->setPassword($pass), 'FTP::setPassword()');
     dump($ftp->login(), 'FTP::login()');
-    // dump($ftp->login('xxx', 'xxx'), 'FTP::login()');
-
     dump($ftp->pwd(), 'FTP::pwd()');
-    
+    dump($ftp->ls(null, NET_FTP_DIRS_ONLY), 'FTP::ls()');
     dump($ftp->mkdir('test'), 'FTP::mkdir()');
-    dump($ftp->rm('test/', true), 'FTP::rm()');
-    
-    dump($ftp->cd('download'), 'FTP::cd()');
-    
-    // dump($ftp->ls(null, NET_FTP_FILES_ONLY), 'FTP::ls()');
-    // dump($ftp->ls(null, NET_FTP_DIRS_ONLY), 'FTP::ls()');
-    dump($ftp->ls(), 'FTP::ls()');
-    
-    // dump($ftp->execute('CHMOD 777 proftpd-1.2.8.tar.gz'), 'FTP::exec()');
-    
-    // dump($ftp->mdtm('proftpd-1.2.8.tar.gz'), 'FTP::mdtm()');
-    dump($ftp->mdtm('proftpd-1.2.8.tar.gz', 'd.m.Y'), 'FTP::mdtm()');
-    
-    dump($ftp->size('proftpd-1.2.8.tar.gz'), 'FTP::size()');
-    
-    dump($ftp->get('proftpd-1.2.8.tar.gz', './proftpd-1.2.8.tar.gz', true, FTP_BINARY), 'FTP::get()');
+    dump($ftp->cd('test'), 'FTP::cd()');
+    dump($ftp->put('proftpd-1.2.8.tar.gz', 'proftpd-1.2.8.tar.gz'), 'FTP::upload()');
+    dump($ftp->ls(null, NET_FTP_FILES_ONLY), 'FTP::ls()');
+    dump($ftp->put('proftpd-1.2.8.tar.gz', '../proftpd-1.2.8.tar.gz'), 'FTP::upload()');
+    dump($ftp->get('proftpd-1.2.8.tar.gz', 'proftpd-1.2.8.tar.gz', true, FTP_BINARY), 'FTP::get()');
+    dump($ftp->ls(null, NET_FTP_FILES_ONLY), 'FTP::ls()');
     dump($ftp->cd('..'), 'FTP::cd()');
-    
-    // dump($ftp->getRecursive('download/', 'download_remote/'), 'FTP::getRecursive()');
-    // dump($ftp->putRecursive('download_remote/', 'uploaded/', true), 'FTP::putRecursive()');
-    
-    dump($ftp->rm('uploaded/', true), 'FTP::rm()');
-    
-    
+    dump($ftp->chmodRecursive('test', 777), 'FTP::chmodRecursive()');
+    dump($ftp->ls(null, NET_FTP_DIRS_ONLY), 'FTP::ls()');
+    dump($ftp->mdtm('proftpd-1.2.8.tar.gz', 'd.m.Y'), 'FTP::mdtm()');
+    dump($ftp->size('proftpd-1.2.8.tar.gz'), 'FTP::size()');
+    dump($ftp->rm('test/', true), 'FTP::rm()');
+    dump($ftp->ls(null, NET_FTP_DIRS_ONLY), 'FTP::ls()');
     dump($ftp->disconnect(), 'FTP::disconnect()');
     
     
