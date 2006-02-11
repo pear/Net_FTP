259578ce461cd9cedd3a2ee1bc33e028a144181f
Tobias Schlitt <toby@php.net>
2006-02-11 10:58:55 +0000

    * Updated generate_package_xml.php to use PEAR_PackageFileManager2. * Updated package.xml and package2.xml for release 1.3.1.
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_FTP/trunk@206953 c90b9560-bf6c-de11-be94-00142212c4b1

--- generate_package_xml.php
+++ generate_package_xml.php
@@ -1,20 +1,20 @@
 <?php
 
-	$make = false;
-	require_once('PEAR/PackageFileManager.php');
+require_once 'PEAR/PackageFileManager2.php';
 
-	$pkg = new PEAR_PackageFileManager;
+function dumpError($err) {
+    var_dump($err);
+    die();
+}
 
-	// directory that PEAR CVS is located in
-	$cvsdir  = '/cvs/pear/';
-	$packagedir = $cvsdir . 'Net_FTP/';
-	$category = 'Networking';	
+$cvsdir  = '/cvs/pear/';
+$packagedir = $cvsdir . 'Net_FTP/';
 
-	$version = '1.3.0';
-	
-	$summary = 'Net_FTP provides an OO interface to the PHP FTP functions plus some additions';
-	
-	$description = <<<EOT
+$version = '1.3.1';
+
+$summary = 'Net_FTP provides an OO interface to the PHP FTP functions plus some additions';
+
+$description = <<<EOT
 Net_FTP allows you to communicate with FTP servers in a more comfortable way
 than the native FTP functions of PHP do. The class implements everything nativly
 supported by PHP and additionally features like recursive up- and downloading,
@@ -23,86 +23,80 @@ for example the view of a progress bar.
 EOT;
 	
 	$notes = <<<EOT
-Another month of testing did not bring any more critical bugs to daylight. Therefore 
-and because PEAR 1.4.0 will depend optionally on Net_FTP I decided to run the next 
-stable release. The following changes were made to the package files:
-
- - Updated year.
- - Updated docblocks regarding the new standard.
- - Fixed whitespace issues.
- - Fixed bug 3362: bug in Net_FTP::_rm_file.
- - Added PEAR 1.4 compatible package2.xml (package.xml version 2.0).
+* Fixed Bug #4102: Problem detecting os method ls().
+* Fixed Bug #5337: _list_and_parse behavior with an empty remote directory.
+* Fixed Bug #4836: Off-by-one error in regex for Windows directory listings.
+* Fixed Bug #4749: ls() fails when connection is closed.
+* Fixed Bug #4969: Recursive rm ends in endless loop.
+* Fixed Bug #5895: Recursive chmod ends in endless loop.
+* Fixed Bug #4009: _determine_os_match doesn't take into account numbered users and groups.
+* Fixed Bug #4008: _list_and_parse tries to determine OS on an empty list.
+* Fixed Bug #3778: Notice of Uninitialized string offset in function _rm_dir_recursive.
 EOT;
-	
-	$e = $pkg->setOptions(
-		array('baseinstalldir' => '',
-		      'summary' => $summary,
-		      'description' => $description,
-		      'version' => $version,
-	          'packagedirectory' => $packagedir,
-	          'pathtopackagefile' => $packagedir,
-              'state' => 'stable',
-              'filelistgenerator' => 'cvs',
-              'notes' => $notes,
-			  'package' => 'Net_FTP',
-			  'dir_roles' => array(
-			  		'example' => 'doc',
-			  		'test' => 'test'),
-		      'ignore' => array('package.xml',
-		                        'doc*',
-		                        'test*', 
-		                        'generate_package_xml.php',
-		                        '*.tgz',
-		                        'FTP_PHP5.php',
-		                        ),
-	));
-	
-	if (PEAR::isError($e)) {
-    	echo $e->getMessage();
-    	exit;
-	}
-	
-	$e = $pkg->addMaintainer('toby', 'lead', 'Tobias Schlitt', 'toby@php.net');
-	
-	
-	if (PEAR::isError($e)) {
-    	echo $e->getMessage();
-    	exit;
-	}
-		
-	$e = $pkg->addDependency('ftp', null, 'has', 'ext');
-	
-	if (PEAR::isError($e)) {
-    	echo $e->getMessage();
-    	exit;
-	}
-	
-	// hack until they get their shit in line with docroot role
-	$pkg->addRole('tpl', 'php');
-	$pkg->addRole('png', 'php');
-	$pkg->addRole('gif', 'php');
-	$pkg->addRole('jpg', 'php');
-	$pkg->addRole('css', 'php');
-	$pkg->addRole('js', 'php');
-	$pkg->addRole('ini', 'php');
-	$pkg->addRole('inc', 'php');
-	$pkg->addRole('afm', 'php');
-	$pkg->addRole('pkg', 'doc');
-	$pkg->addRole('cls', 'doc');
-	$pkg->addRole('proc', 'doc');
-	$pkg->addRole('sh', 'script');
-	
-	if (isset($make)) {
-    	$e = $pkg->writePackageFile();
-	} else {
-    	$e = $pkg->debugPackageFile();
-	}
-	
-	if (PEAR::isError($e)) {
-    	echo $e->getMessage();
-	}
-	
-	if (!isset($make)) {
-    	echo '<a href="' . $_SERVER['PHP_SELF'] . '?make=1">Make this file</a>';
-	}
+
+PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, 'dumpError');
+
+$p2 = new PEAR_PackageFileManager2();
+$p2->setOptions(
+    array(
+        'baseinstalldir'    => '/',
+        'filelistgenerator' => 'cvs',
+        'packagedirectory'  => dirname(__FILE__),
+        'include'           => array(),
+        'ignore'            => array(
+            'package.xml',
+            'package2.xml',
+            '*.tgz',
+            'generate*',
+            'doc*',
+            'FTP_PHP5.php',
+        ),
+        'dir_roles'         => array(
+            'tests'     => 'test',
+            'example'   => 'doc'
+        ),
+        'simpleoutput'      => true,
+    )
+);
+
+$p2->setPackage('Net_FTP');
+$p2->setSummary($summary);
+$p2->setDescription($description);
+$p2->setChannel('pear.php.net');
+
+$p2->setPackageType('php');
+
+$p2->generateContents();
+
+$p2->setReleaseVersion($version);
+$p2->setAPIVersion('1.0.0');
+$p2->setReleaseStability('stable');
+$p2->setAPIStability('stable');
+
+$p2->setNotes($notes);
+
+$p2->addGlobalReplacement('package-info', '@package_version@', 'version');
+
+$p2->addRelease();
+
+$p2->addMaintainer('lead', 'toby', 'Tobias Schlitt', 'toby@php.net');
+
+$p2->setPhpDep('4.3.0');
+$p2->setPearinstallerDep('1.3.0');
+
+$p2->setLicense('PHP License', 'http://www.php.net/license');
+
+$p2->addExtensionDep('required', 'ftp');
+
+$p1 =& $p2->exportCompatiblePackageFile1();
+
+if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
+    echo "Writing package file\n";
+    $p2->writePackageFile();
+    $p1->writePackageFile();
+} else {
+    echo "Debugging package file\n";
+    $p2->debugPackageFile();
+    $p1->debugPackageFile();
+}
 ?>
