b8ef981a58645347e09f4b486be271f46d791226
Tom Sommer <mail@tomsommer.dk>
2015-01-20 12:59:31 +0100

    Validate return of _lsFiles() and _lsDirs() during _rmDirRecursive()

--- Net/FTP.php
+++ Net/FTP.php
@@ -2345,6 +2345,10 @@ class Net_FTP extends PEAR
                                      NET_FTP_ERR_REMOTEPATHNODIR);
         }
         $file_list = $this->_lsFiles($dir);
+        if (PEAR::isError($file_list)) {
+            return $file_list;
+        }
+        
         foreach ($file_list as $file) {
             $file = $dir.$file["name"];
             $res  = $this->rm($file);
@@ -2352,7 +2356,12 @@ class Net_FTP extends PEAR
                 return $res;
             }
         }
+        
         $dir_list = $this->_lsDirs($dir);
+        if (PEAR::isError($dir_list)) {
+            return $dir_list;
+        }
+        
         foreach ($dir_list as $new_dir) {
             if ($new_dir["name"] == '.' || $new_dir["name"] == '..') {
                 continue;
